0e38a0092a93ed7ed89fcea9330f098fca76601e
Chryz Eduard Q. Mamugos <95526590+ChryzEduard@users.noreply.github.com>
2024-03-14 14:52:35 +0800

    Update app.py

--- app.py
+++ app.py
@@ -87,7 +87,7 @@ with c2:
 
 
 predict_call = st.button("Predict")
-model = pickle.load(open("rfr_student_grade_modelv4.pkl.pkl", "rb"))
+model = pickle.load(open("rfr_student_grade_modelv4.pkl", "rb"))
 print(model)
 
 
